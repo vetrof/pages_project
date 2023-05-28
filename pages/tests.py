e60af53a55a927ea1317dd6aa488a6871c7b7b81
Vitaly Vetrof <vetrof@gmail.com>
2023-05-28 12:44:29 +0600

    update

---
+++ pages/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase, SimpleTestCase
+
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEquals(response.status_code, 200)
+
---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from pages.views import HomePageView, AboutPageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+]
